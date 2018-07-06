65edcebf6385777e4c8901b3f5e36e3ab1e2dea8
Charalampos Mainas <chmainas@cslab.ece.ntua.gr>
2018-07-06 14:30:44 +0300

    No interrupts

--- rumprun/stage3/ivshmem.c
+++ rumprun/stage3/ivshmem.c
@@ -52,7 +52,7 @@ CFATTACH_DECL_NEW(ivshmem, sizeof(struct ivshmem_softc), ivshmem_match,
 static int ivshmem_match(device_t dev, cfdata_t cf, void *v)
 {
 	struct pci_attach_args *pa;
-	printf("IVSHMEM: Hello form ivshmem_match\n");
+	//printf("IVSHMEM: Hello form ivshmem_match\n");
 	pa = (struct pci_attach_args *) v;
 	if (PCI_VENDOR(pa->pa_id) == PCI_VENDOR_QUMRANET) {
 		printf("IVSHMEM: Found match\n");
@@ -74,23 +74,29 @@ static void ivshmem_attach(device_t parent, device_t self, void *v)
 	bus_space_handle_t ioh;
 	bus_size_t iosize; 
 	//char intrbuf[PCI_INTRSTR_LEN];
-
 	pci_chipset_tag_t pc = pa->pa_pc;
 	pcitag_t tag = pa->pa_tag;
+	pcireg_t csr;
 	//pcireg_t id = pci_conf_read(pc, tag, PCI_SUBSYS_ID_REG);
+	
 	pci_aprint_devinfo(pa, NULL);
+	csr = pci_conf_read(pc, tag, PCI_COMMAND_STATUS_REG);
+	printf("csr: %08x\n", csr);
+	if ((csr & PCI_COMMAND_MEM_ENABLE) == 0) {
+		aprint_error_dev(self, "memory access is disabled\n");
+		return;
+	}
 	sc->dev = self;
 	sc->pc = pc;
 	sc->tag = tag;
-	if (pci_mapreg_map(pa, PCI_BAR(0), PCI_MAPREG_TYPE_IO, 0, &iot, 
-				&ioh, NULL, &iosize)) {
-		aprint_error_dev(self, "can't map reg\n");
-		return;
-	}
-	sc->reg_tag = iot;
-	sc->reg_handle = ioh;
-	sc->reg_size = iosize;
-	sc->dma_tag = pa->pa_dmat;
+	//if (pci_mapreg_map(pa, PCI_BAR(0), PCI_MAPREG_TYPE_MEM, 0, &iot, 
+	//			&ioh, NULL, &iosize)) {
+	//	aprint_error_dev(self, "can't map reg\n");
+	//	return;
+	//}
+	//sc->reg_tag = iot;
+	//sc->reg_handle = ioh;
+	//sc->reg_size = iosize;
 	/* data region */
 	if (pci_mapreg_map(pa, PCI_BAR(2), PCI_MAPREG_TYPE_IO, 0, &iot, 
 				&ioh, NULL, &iosize)) {
@@ -100,6 +106,12 @@ static void ivshmem_attach(device_t parent, device_t self, void *v)
 	sc->data_tag = iot;
 	sc->data_handle = ioh;
 	sc->data_size = iosize;
+	printf("IVSHMEM: tag %ld\n", iot);
+	printf("IVSHMEM: handle %ld\n", ioh);
+	printf("IVSHMEM: size %ld\n", iosize);
+	bus_space_write_4(sc->data_tag, sc->data_handle, 0, 77);
+	int tmp = bus_space_read_4(sc->data_tag, sc->data_handle, 0);
+	printf("IVSHMEM: read %d\n", tmp);
 	/* interrupts */
 	return;
 }
@@ -111,12 +123,14 @@ static int ivshmem_detach(device_t dev, int flags)
 	struct ivshmem_softc *sc = device_private(dev);
 	printf("IVSHMEM: Hello from ivshmem_detach\n");
 	/* Teardown the state in our softc created in our attach routine. */
-	if (sc->reg_size)
-		bus_space_unmap(sc->reg_tag, sc->reg_handle, sc->reg_size);
-	sc->reg_size = 0;
-	if (sc->data_size)
+	//if (sc->reg_size) {
+	//	bus_space_unmap(sc->reg_tag, sc->reg_handle, sc->reg_size);
+	//	sc->reg_size = 0;
+	//}
+	if (sc->data_size) {
 		bus_space_unmap(sc->data_tag, sc->data_handle, sc->data_size);
-	sc->data_size = 0;
+		sc->data_size = 0;
+	}
 	return 0;
 }
 
